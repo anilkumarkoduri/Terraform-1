07d83d893e4c53e36e59ad9f5666cbb56b66575b
Raghu K <raghuk.vit@gmail.com>
2019-01-24 06:54:29 +0530

    ss

--- 02-vars/vpc.tf
+++ 02-vars/vpc.tf
@@ -2,6 +2,9 @@ resource "aws_vpc" "main" {
   cidr_block = "${var.vpc-cidr-block}"
 
   tags = {
-    Name = "${var.tags["Project"]}-VPC"
+    Name = "${var.tags["Project"]}-VPC"  ## <<- We are referring maps 
+    Project-Name = "${var.tags["Project"]}"
+    Project-ENV = "${var.tags["Environment"]}"
+    Created-By = "${var.tags["created-by"]}"
   }
 }
