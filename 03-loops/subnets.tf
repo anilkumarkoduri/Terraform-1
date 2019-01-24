c9d2d32f78125f9d532cce1e11b9730a2f34da7f
Raghu K <raghuk.vit@gmail.com>
2019-01-24 07:08:05 +0530

    ss

--- 03-loops/subnets.tf
+++ 03-loops/subnets.tf
@@ -3,4 +3,10 @@ resource "aws_subnet" "subnets" {
   vpc_id     = "${aws_vpc.main.id}"  
   cidr_block = "${var.subnet-cidr-blocks[count.index]}"  
   availability_zone = "${var.azs[count.index]}"  
+  tags = {
+    Name = "${var.tags["Project"]}-Subnet-${count.index+1}" 
+    Project-Name = "${var.tags["Project"]}"
+    Project-ENV = "${var.tags["Environment"]}"
+    Created-By = "${upper(var.tags["created-by"])}"
+  }
 }
