a3eb9ff55ef684ff077842b245d47e6061c4419b
Raghu K <raghuk.vit@gmail.com>
2019-01-25 06:19:32 +0530

    ss

--- 04-data/subnets.tf
+++ 04-data/subnets.tf
@@ -2,7 +2,7 @@ resource "aws_subnet" "subnets" {
   count=3
   vpc_id     = "${aws_vpc.main.id}"  
   cidr_block = "${var.subnet-cidr-blocks[count.index]}"  
-  availability_zone = "${data.aws_availability_zones.azs[count.index]}"  
+  availability_zone = "${data.aws_availability_zones.azs.names[count.index]}"  
   tags = {
     Name = "${var.tags["Project"]}-Subnet-${count.index+1}" 
     Project-Name = "${var.tags["Project"]}"
