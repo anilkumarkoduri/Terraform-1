d3370b367b6593160d9ee3a7711d9820e3f20316
Raghu K <raghuk.vit@gmail.com>
2019-01-25 06:17:58 +0530

    ss

---
+++ 04-data/subnets.tf
@@ -0,0 +1,12 @@
+resource "aws_subnet" "subnets" {
+  count=3
+  vpc_id     = "${aws_vpc.main.id}"  
+  cidr_block = "${var.subnet-cidr-blocks[count.index]}"  
+  availability_zone = "${data.aws_availability_zones.azs[count.index]}"  
+  tags = {
+    Name = "${var.tags["Project"]}-Subnet-${count.index+1}" 
+    Project-Name = "${var.tags["Project"]}"
+    Project-ENV = "${var.tags["Environment"]}"
+    Created-By = "${upper(var.tags["created-by"])}"
+  }
+}
---
+++ 04-data/vars.tf
@@ -0,0 +1,21 @@
+variable "vpc-cidr-block" {
+  type = "string"
+  default = "10.0.0.0/21"
+}
+
+variable "subnet-cidr-blocks" {
+  type = "list"
+  default = ["10.0.0.0/24","10.0.1.0/24","10.0.2.0/24"]
+} 
+
+data "aws_availability_zones" "azs" {}
+
+variable "tags" {
+  type = "map"
+
+  default = {
+    created-by = "Terraform"
+    Project = "Test"
+    Environment = "DEV"
+  }
+}
