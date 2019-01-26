a24d1ef8bf1486b171a550d9c29f18535d506ec1
Raghu K <raghuk.vit@gmail.com>
2019-01-26 07:33:07 +0530

    --- Auto-Git Commit ---

--- stack/Modules/Network/route-tables.tf
+++ stack/Modules/Network/route-tables.tf
@@ -3,7 +3,7 @@ resource "aws_route_table" "public-rt" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.main.id}"
+    gateway_id = "${aws_internet_gateway.igw.id}"
   }
 
 
