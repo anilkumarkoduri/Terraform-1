6bc7cf50658a792ad99fbde9e06306f08676da2c
Raghu K <raghuk.vit@gmail.com>
2019-01-26 07:24:53 +0530

    --- Auto-Git Commit ---

--- stack/Modules/Network/vars.tf
+++ stack/Modules/Network/vars.tf
@@ -1,7 +1,11 @@
 variable "ENV" {}
 variable "PROJECT_NAME" {}
 variable "VPC_CIDR"   {}
+variable "PUBLIC_SUBNET_CIDR" {
+    type = "list"
+}
+
 variable "PRIVATE_SUBNET_CIDR" {
     type = "list"
-}  
+}
 variable "SUBNET_COUNT" {}
