62d55787e18f7089142c7036dad24abe02a4e9fd
Raghu K <raghuk.vit@gmail.com>
2019-01-26 07:24:04 +0530

    --- Auto-Git Commit ---

--- stack/modules.tf
+++ stack/modules.tf
@@ -3,6 +3,7 @@ module "Network" {
   ENV               = "${var.ENV}"
   PROJECT_NAME      = "${var.PROJECT_NAME}"
   VPC_CIDR          = "${var.VPC_CIDR}"
-  SUBNET_CIDR       = "${var.SUBNET_CIDR}"
+  PRIVATE_SUBNET_CIDR       = "${var.PRIVATE_SUBNET_CIDR}"
+  PUBLIC
   SUBNET_COUNT      = "${var.SUBNET_COUNT}"
 }
