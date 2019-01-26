b3fae81c9db232145d048cd8e1d73a57c22b9fcd
Raghu K <raghuk.vit@gmail.com>
2019-01-26 07:21:33 +0530

    --- Auto-Git Commit ---

--- stack/Modules/Network/igw.tf
+++ stack/Modules/Network/igw.tf
@@ -2,7 +2,7 @@ resource "aws_internet_gateway" "igw" {
   vpc_id = "${aws_vpc.main.id}"
 
   tags = {
-    Name            = "${var.PROJECT_NAME}-${var.ENV}-VPC"
+    Name            = "${var.PROJECT_NAME}-${var.ENV}-IGW"
     Project-ENV     = "${var.ENV}"
     Project-NAME    = "${var.PROJECT_NAME}"
     Created-By      = "Terraform"
