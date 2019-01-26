b77c6d006d7651754c8aa5c552a85c8b207b33e3
Raghu K <raghuk.vit@gmail.com>
2019-01-26 07:18:32 +0530

    --- Auto-Git Commit ---

--- stack/Modules/Network/vpc.tf
+++ stack/Modules/Network/vpc.tf
@@ -2,10 +2,10 @@ resource "aws_vpc" "main" {
   cidr_block = "${var.VPC_CIDR}"
 
   tags = {
-    Name        = "${var.PROJECT_NAME}-${var.ENV}-VPC"
-    Project-ENV = "${var.ENV}"
-    Project-NAME = "${var.PROJECT_NAME}"
-    Created-By    = "Terraform"
+    Name            = "${var.PROJECT_NAME}-${var.ENV}-VPC"
+    Project-ENV     = "${var.ENV}"
+    Project-NAME    = "${var.PROJECT_NAME}"
+    Created-By      = "Terraform"
   }
 }
 
