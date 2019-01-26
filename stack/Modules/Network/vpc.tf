35201eb1c2371a06f71d898cbdb8ed2f55c86a8c
Raghu K <raghuk.vit@gmail.com>
2019-01-26 07:00:13 +0530

    --- Auto-Git Commit ---

--- stack/Modules/Network/vpc.tf
+++ stack/Modules/Network/vpc.tf
@@ -2,7 +2,6 @@ resource "aws_vpc" "main" {
   cidr_block = "${var.vpc-cidr-block}"
 
   tags = {
-    Project-ENV = "${var.tags["Environment"]}"
-    Created-By = "${upper(var.tags["created-by"])}"
+    Project-ENV = "${var.ENV}"
   }
 }
