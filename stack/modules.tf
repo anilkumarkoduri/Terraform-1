80677974468df1741e7a92b7048f9cec5d7b40bc
Raghu K <raghuk.vit@gmail.com>
2019-01-26 07:11:31 +0530

    --- Auto-Git Commit ---

--- stack/modules.tf
+++ stack/modules.tf
@@ -2,5 +2,6 @@ module "Network" {
   source            = "./Modules/Network"
   ENV               = "${var.ENV}"
   PROJECT_NAME      = "${var.PROJECT_NAME}"
-  VPC_CIDR          = "${var.}"
+  VPC_CIDR          = "${var.VPC_CIDR}"
+  
 }
