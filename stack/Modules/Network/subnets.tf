b36747f4c2b4e9d6798fcf094ef079648c6f0690
Raghu K <raghuk.vit@gmail.com>
2019-01-26 07:30:44 +0530

    --- Auto-Git Commit ---

--- stack/Modules/Network/subnets.tf
+++ stack/Modules/Network/subnets.tf
@@ -10,3 +10,16 @@ resource "aws_subnet" "public-subnets" {
     Created-By      = "Terraform"
   }
 }
+
+resource "aws_subnet" "public-subnets" {
+    count           = "${length(var.PUBLIC_SUBNET_CIDR)}"
+    vpc_id          = "${aws_vpc.main.id}"
+    cidr_block      = "${element(var.PUBLIC_SUBNET_CIDR, count.index)}"
+
+  tags = {
+    Name            = "${var.PROJECT_NAME}-${var.ENV}-Public-Subnet-${count.index+1}"
+    Project-ENV     = "${var.ENV}"
+    Project-NAME    = "${var.PROJECT_NAME}"
+    Created-By      = "Terraform"
+  }
+}
