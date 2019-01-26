36c0575812d0ce0b563d0aa3de9a501e77997763
Raghu K <raghuk.vit@gmail.com>
2019-01-26 07:37:07 +0530

    --- Auto-Git Commit ---

--- stack/Modules/Network/route-tables.tf
+++ stack/Modules/Network/route-tables.tf
@@ -27,6 +27,6 @@ resource "aws_route_table" "private-rt" {
 
 resource "aws_route_table_association" "public-rt-assoc" {
     count = "${length(var.PUBLIC_SUBNET_CIDR)}"
-    subnet_id      = "${element(aws_subnet.foo.*.id, count.index)}"
+    subnet_id      = "${element(aws_subnet.public-subnets.*.id, count.index)}"
     route_table_id = "${aws_route_table.public-rt.id}"
 }
