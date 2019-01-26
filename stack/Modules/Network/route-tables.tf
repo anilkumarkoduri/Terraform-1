1605fdc00f4e62b959ff413c844cc427725bb43c
Raghu K <raghuk.vit@gmail.com>
2019-01-26 07:33:44 +0530

    --- Auto-Git Commit ---

--- stack/Modules/Network/route-tables.tf
+++ stack/Modules/Network/route-tables.tf
@@ -13,3 +13,19 @@ resource "aws_route_table" "public-rt" {
     Created-By      = "Terraform"
   }
 }
+
+resource "aws_route_table" "private-rt" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.igw.id}"
+  }
+
+  tags = {
+    Name            = "${var.PROJECT_NAME}-${var.ENV}-Public-RT"
+    Project-ENV     = "${var.ENV}"
+    Project-NAME    = "${var.PROJECT_NAME}"
+    Created-By      = "Terraform"
+  }
+}
