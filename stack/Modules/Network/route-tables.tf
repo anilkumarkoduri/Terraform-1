b0e1212ceb6422e99ef5cce31975c6a2f8f1c772
Raghu K <raghuk.vit@gmail.com>
2019-01-26 07:35:45 +0530

    --- Auto-Git Commit ---

--- stack/Modules/Network/route-tables.tf
+++ stack/Modules/Network/route-tables.tf
@@ -24,3 +24,8 @@ resource "aws_route_table" "private-rt" {
     Created-By      = "Terraform"
   }
 }
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = "${aws_subnet.foo.id}"
+  route_table_id = "${aws_route_table.bar.id}"
+}
