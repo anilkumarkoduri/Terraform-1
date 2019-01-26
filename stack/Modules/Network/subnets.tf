f249812a8761603489e9ef69673f821122427ae9
Raghu K <raghuk.vit@gmail.com>
2019-01-26 07:29:14 +0530

    --- Auto-Git Commit ---

--- stack/Modules/Network/subnets.tf
+++ stack/Modules/Network/subnets.tf
@@ -4,6 +4,9 @@ resource "aws_subnet" "main" {
     cidr_block      = "${element(var.PUBLIC_SUBNET_CIDR, count.index)}"
 
   tags = {
-   
+    Name            = "${var.PROJECT_NAME}-${var.ENV}-IGW"
+    Project-ENV     = "${var.ENV}"
+    Project-NAME    = "${var.PROJECT_NAME}"
+    Created-By      = "Terraform"
   }
 }
