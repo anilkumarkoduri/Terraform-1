353cb826a7f4d51b308cbbd1216fa8361590168c
Raghu K <raghuk.vit@gmail.com>
2019-01-26 07:31:06 +0530

    --- Auto-Git Commit ---

--- stack/Modules/Network/subnets.tf
+++ stack/Modules/Network/subnets.tf
@@ -14,10 +14,10 @@ resource "aws_subnet" "public-subnets" {
 resource "aws_subnet" "private-subnets" {
     count           = "${length(var.PRIVATE_SUBNET_CIDR)}"
     vpc_id          = "${aws_vpc.main.id}"
-    cidr_block      = "${element(var.PUBLIC_SUBNET_CIDR, count.index)}"
+    cidr_block      = "${element(var.PRIVATE_SUBNET_CIDR, count.index)}"
 
   tags = {
-    Name            = "${var.PROJECT_NAME}-${var.ENV}-Public-Subnet-${count.index+1}"
+    Name            = "${var.PROJECT_NAME}-${var.ENV}-Private-Subnet-${count.index+1}"
     Project-ENV     = "${var.ENV}"
     Project-NAME    = "${var.PROJECT_NAME}"
     Created-By      = "Terraform"
