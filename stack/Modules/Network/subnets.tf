d9302c7416499e2a38ab468a8f08ad877a07bf89
Raghu K <raghuk.vit@gmail.com>
2019-01-28 06:47:43 +0530

    --- Auto-Git Commit ---

--- stack/Modules/Network/subnets.tf
+++ stack/Modules/Network/subnets.tf
@@ -3,6 +3,7 @@ resource "aws_subnet" "public-subnets" {
     vpc_id          = "${aws_vpc.main.id}"
     cidr_block      = "${element(var.PUBLIC_SUBNET_CIDR, count.index)}"
     availability_zone = "${data.aws_availability_zones.azs.names[count.index]}"
+    
   tags = {
     Name            = "${var.PROJECT_NAME}-${var.ENV}-Public-Subnet-${count.index+1}"
     Project-ENV     = "${var.ENV}"
@@ -15,6 +16,7 @@ resource "aws_subnet" "private-subnets" {
     count           = "${length(var.PRIVATE_SUBNET_CIDR)}"
     vpc_id          = "${aws_vpc.main.id}"
     cidr_block      = "${element(var.PRIVATE_SUBNET_CIDR, count.index)}"
+    availability_zone = "${data.aws_availability_zones.azs.names[count.index]}"
 
   tags = {
     Name            = "${var.PROJECT_NAME}-${var.ENV}-Private-Subnet-${count.index+1}"
