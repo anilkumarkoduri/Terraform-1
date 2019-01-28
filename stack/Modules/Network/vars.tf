b80a333de5b1e9883f016d6dbdc85a849a3c9dbc
Raghu K <raghuk.vit@gmail.com>
2019-01-28 06:46:23 +0530

    --- Auto-Git Commit ---

--- stack/Modules/Network/vars.tf
+++ stack/Modules/Network/vars.tf
@@ -12,4 +12,4 @@ variable "PRIVATE_SUBNET_CIDR" {
 variable "MANAGEMENT_VPC" {}
 
 variable "MANAGEMENT_CIDR" {} 
-data "aws_availability_zones" "available" {}
+data "aws_availability_zones" "azs" {}
