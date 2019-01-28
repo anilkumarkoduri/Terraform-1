6545aaa7ce224f97ae3f0a6d88b4af565ad61dc6
Raghu K <raghuk.vit@gmail.com>
2019-01-28 06:47:04 +0530

    --- Auto-Git Commit ---

--- stack/Modules/Network/subnets.tf
+++ stack/Modules/Network/subnets.tf
@@ -2,7 +2,7 @@ resource "aws_subnet" "public-subnets" {
     count           = "${length(var.PUBLIC_SUBNET_CIDR)}"
     vpc_id          = "${aws_vpc.main.id}"
     cidr_block      = "${element(var.PUBLIC_SUBNET_CIDR, count.index)}"
-
+    availability_zone = ""
   tags = {
     Name            = "${var.PROJECT_NAME}-${var.ENV}-Public-Subnet-${count.index+1}"
     Project-ENV     = "${var.ENV}"
