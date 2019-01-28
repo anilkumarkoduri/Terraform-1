b4246247648b35d42dd6856bd14ff27adcaa77b0
Raghu K <raghuk.vit@gmail.com>
2019-01-28 06:39:42 +0530

    --- Auto-Git Commit ---

--- stack/modules.tf
+++ stack/modules.tf
@@ -13,7 +13,7 @@ module "Network" {
   source                = "./Modules/Database"
   ENV                   = "${var.ENV}"
   PROJECT_NAME          = "${var.PROJECT_NAME}"
-  VPC_ID            = "${module.Network.vpc_id}"
-  PUBLIC_SUBNETS    = "${module.Network.public_subnets}"
-  PRIVATE_SUBNETS    = "${module.Network.private_subnets}"
+  VPC_ID                = "${module.Network.vpc_id}"
+  PUBLIC_SUBNETS        = "${module.Network.public_subnets}"
+  PRIVATE_SUBNETS       = "${module.Network.private_subnets}"
 }
