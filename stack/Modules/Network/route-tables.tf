21aa6287fd858aa20e273fb9b38ac099da0ea18a
Raghu K <raghuk.vit@gmail.com>
2019-01-28 06:25:30 +0530

    --- Auto-Git Commit ---

--- stack/Modules/Network/route-tables.tf
+++ stack/Modules/Network/route-tables.tf
@@ -8,7 +8,7 @@ resource "aws_route_table" "public-rt" {
 
   route {
     cidr_block = "${var.MANAGEMENT_CIDR}"
-    vpc_peering_connection_id = "${aws_pee}"
+    vpc_peering_connection_id = "${aws_vpc_peering_connection.peer.id}"
   }
 
   tags = {
