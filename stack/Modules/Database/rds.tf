1fa405a5f5483c56a2581088775bc29b4c38a360
Raghu K <raghuk.vit@gmail.com>
2019-01-30 06:08:59 +0530

    --- Auto-Git Commit ---

--- stack/Modules/Database/rds.tf
+++ stack/Modules/Database/rds.tf
@@ -1,5 +1,5 @@
 resource "aws_db_instance" "default" {
-  allocated_storage    = 10
+  allocated_storage    = 20
   storage_type         = "gp2"
   engine               = "mysql"
   engine_version       = "5.7"
