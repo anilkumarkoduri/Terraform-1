a860e9bb176cba0ced673ed26464ee003881b649
Raghu K <raghuk.vit@gmail.com>
2019-01-30 06:43:44 +0530

    --- Auto-Git Commit ---

--- stack/Modules/Database/security-group.tf
+++ stack/Modules/Database/security-group.tf
@@ -6,8 +6,8 @@ resource "aws_security_group" "rds" {
   ingress {
     from_port   = 3306
     to_port     = 3306
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp"
+    cidr_blocks = ["]
   }
 
   egress {
