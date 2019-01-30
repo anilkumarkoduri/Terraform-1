fa66a1a52d603fc5fbe4af21e4973f13d67bf694
Raghu K <raghuk.vit@gmail.com>
2019-01-30 06:11:29 +0530

    --- Auto-Git Commit ---

--- stack/Modules/Database/rds.tf
+++ stack/Modules/Database/rds.tf
@@ -3,7 +3,7 @@ resource "aws_db_instance" "default" {
   storage_type         = "gp2"
   engine               = "mariadb"
   engine_version       = "10.3"
-  instance_class       = "db.t2.micro"
+  instance_class       = ""
   name                 = "mydb"
   username             = "foo"
   password             = "foobarbaz"
