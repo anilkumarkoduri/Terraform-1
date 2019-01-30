33c385ab8015cf94be26a88caef455d538f5c3a6
Raghu K <raghuk.vit@gmail.com>
2019-01-30 06:19:21 +0530

    --- Auto-Git Commit ---

--- stack/Modules/Database/rds.tf
+++ stack/Modules/Database/rds.tf
@@ -6,5 +6,5 @@ resource "aws_db_instance" "default" {
   instance_class       = "${var.DB_INSTANCE_TYPE}"
   username             = "${var.DBUSER}"
   password             = "${var.DBPASS}"
-  parameter_group_name = "default.mysql5.7"
+  parameter_group_name = "${aws_db_parameter_group.default.id}"
 }
