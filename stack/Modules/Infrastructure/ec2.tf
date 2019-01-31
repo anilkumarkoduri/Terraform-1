4b46096b514d7d9e2539f30a4d44c31ee6a9d881
Raghu K <raghuk.vit@gmail.com>
2019-01-31 06:22:13 +0530

    --- Auto-Git Commit ---

--- stack/Modules/Infrastructure/ec2.tf
+++ stack/Modules/Infrastructure/ec2.tf
@@ -5,7 +5,10 @@ resource "aws_instance" "web" {
     key_name                = "devops"
     subnet_id               = "${element(var.PUBLIC_SUBNETS, count.index)}"
     vpc_security_group_ids  = ["${aws_security_group.ec2-sg.id}"]
-    tags = {
-
+    tags                    = {
+        Name            = "${var.PROJECT_NAME}-${var.ENV}-Instance-${count.index+1}"
+        Project-ENV     = "${var.ENV}"
+        Project-NAME    = "${var.PROJECT_NAME}"
+        Created-By      = "Terraform"
   }
 }
