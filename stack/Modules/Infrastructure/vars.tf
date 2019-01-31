5f5cb1e94d563a647bb761bab6b9cef49a17078c
Raghu K <raghuk.vit@gmail.com>
2019-01-31 06:04:41 +0530

    --- Auto-Git Commit ---

---
+++ stack/Modules/Infrastructure/vars.tf
@@ -0,0 +1,11 @@
+variable "ENV" {}
+variable "PROJECT_NAME" {}
+
+variable "VPC_ID" {}
+variable "VPC_CIDR" {}
+variable "PUBLIC_SUBNETS"  {
+    type    = "list"
+}
+variable "PRIVATE_SUBNETS" {
+    type    = "list"
+}
