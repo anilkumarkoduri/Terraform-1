e01bcae6bc83e9f65aae98b710c582cfe9388383
Raghu K <raghuk.vit@gmail.com>
2019-01-31 06:21:05 +0530

    --- Auto-Git Commit ---

--- stack/Modules/Infrastructure/ec2.tf
+++ stack/Modules/Infrastructure/ec2.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "web" {
-    count           = 2
-    ami             = "${var.AMI_ID}"
-    instance_type   = "${var.INSTANCE_TYPE}"
-    key_name        = "devops"
-    subnet_id       = "${element(var.PUBLIC_SUBNETS, count.index)}"
-    
+    count                   = 2
+    ami                     = "${var.AMI_ID}"
+    instance_type           = "${var.INSTANCE_TYPE}"
+    key_name                = "devops"
+    subnet_id               = "${element(var.PUBLIC_SUBNETS, count.index)}"
+    vpc_security_group_ids  = ["${aws_security_group.ec2-sg.id}"]
     tags = {
 
   }
