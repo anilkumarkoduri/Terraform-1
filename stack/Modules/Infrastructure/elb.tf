5543171ecebed3bd35f2f5c8afa6ceda2840f609
Raghu K <raghuk.vit@gmail.com>
2019-02-02 07:01:14 +0530

    --- Auto-Git Commit ---

--- stack/Modules/Infrastructure/elb.tf
+++ stack/Modules/Infrastructure/elb.tf
@@ -15,7 +15,7 @@ resource "aws_elb" "studentapp-elb" {
     healthy_threshold   = 2
     unhealthy_threshold = 2
     timeout             = 3
-    target              = "HTTP:80/"
+    target              = "TCP:80"
     interval            = 30
   }
 
