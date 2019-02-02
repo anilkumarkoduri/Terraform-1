6095bff44dc5bdf76997e8938696e7902b9dc51c
Raghu K <raghuk.vit@gmail.com>
2019-02-02 08:02:42 +0530

    --- Auto-Git Commit ---

--- stack/Modules/Database/subnet-groups.tf
+++ stack/Modules/Database/subnet-groups.tf
@@ -6,7 +6,8 @@ resource "aws_db_subnet_group" "default" {
 resource "aws_db_parameter_group" "default" {
   name   = "rds-studentapp-pg"
   family = "mariadb10.3"
-  apply_method = ""
+  apply_method = "pending-reboot"
+  
   parameter {
     name  = "lower_case_table_names"
     value = "1"
