d8b9b3f6580548a28d9cf98b0c27bc366c72b4ea
Raghu K <raghuk.vit@gmail.com>
2019-02-03 07:04:42 +0530

    --- Auto-Git Commit ---

--- stack-asg/Modules/Infrastructure/iam-role.tf
+++ stack-asg/Modules/Infrastructure/iam-role.tf
@@ -1,31 +1,4 @@
-resource "aws_iam_policy" "s3-access-policy" {
-  name        = "EC2-Accessing-S3-Terraform"
-  path        = "/"
-  description = "EC2-Accessing-S3-Terraform"
 
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "VisualEditor0",
-            "Effect": "Allow",
-            "Action": "s3:ListBucket",
-            "Resource": "arn:aws:s3:::terra-citb34"
-        },
-        {
-            "Sid": "VisualEditor1",
-            "Effect": "Allow",
-            "Action": [
-                "s3:GetObject",
-                "s3:PutObjectAcl"
-            ],
-            "Resource": "arn:aws:s3:::terra-citb34/*"
-        }
-    ]
-}
-EOF
-}
 
 resource "aws_iam_role" "test_role" {
   name = "test_role"
