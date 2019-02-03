f3aa1eef6105275b3b6d2983d1780849397f7512
Raghu K <raghuk.vit@gmail.com>
2019-02-03 07:35:26 +0530

    --- Auto-Git Commit ---

--- stack-asg/Modules/Infrastructure/iam-role.tf
+++ stack-asg/Modules/Infrastructure/iam-role.tf
@@ -20,7 +20,7 @@ EOF
 
 resource "aws_iam_instance_profile" "ec2-profile" {
   name = "ec2-instance-profile-studentapp-dev"
-  roles = ["iam-role-for-ec2"]
+  role = ["${}"]
 }
 
 resource "aws_iam_role_policy" "ec2-role-policy" {
