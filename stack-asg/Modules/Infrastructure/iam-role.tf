79434525c743e440d1658376b1e2a572368869bf
Raghu K <raghuk.vit@gmail.com>
2019-02-03 07:05:32 +0530

    --- Auto-Git Commit ---

--- stack-asg/Modules/Infrastructure/iam-role.tf
+++ stack-asg/Modules/Infrastructure/iam-role.tf
@@ -1,6 +1,20 @@
 resource "aws_iam_role" "ec2-role" {
   name = "EC2-Access-to-S3-Terraform-Bucket"
-  policy 
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF 
   tags = {
       tag-key = "tag-value"
   }
