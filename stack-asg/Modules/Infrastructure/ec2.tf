b18a633e09fd3f28ca0813bb1eb2cba5fb953aea
Raghu K <raghuk.vit@gmail.com>
2019-02-03 07:12:13 +0530

    --- Auto-Git Commit ---

--- stack-asg/Modules/Infrastructure/ec2.tf
+++ stack-asg/Modules/Infrastructure/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_launch_configuration" "launch-config" {
   name                  = "student-config"
   image_id              = "${var.AMI_ID}"
   instance_type         = "${var.INSTANCE_TYPE}"
-
+  
 
 }
 
