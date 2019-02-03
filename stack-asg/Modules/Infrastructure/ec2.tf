f1e42a15866a6dbd6f02a81f3a87d100e6d7e477
Raghu K <raghuk.vit@gmail.com>
2019-02-03 07:16:13 +0530

    --- Auto-Git Commit ---

--- stack-asg/Modules/Infrastructure/ec2.tf
+++ stack-asg/Modules/Infrastructure/ec2.tf
@@ -3,6 +3,7 @@ resource "aws_launch_configuration" "launch-config" {
   image_id              = "${var.AMI_ID}"
   instance_type         = "${var.INSTANCE_TYPE}"
   iam_instance_profile  = "${aws_iam_instance_profile.ec2-profile.name}"
+  key_name              
 
 }
 
