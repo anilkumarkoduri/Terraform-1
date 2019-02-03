33c5a5b11aca020e8b603117dbcc49b01176729e
Raghu K <raghuk.vit@gmail.com>
2019-02-03 07:37:07 +0530

    --- Auto-Git Commit ---

--- stack-asg/Modules/Infrastructure/iam-role.tf
+++ stack-asg/Modules/Infrastructure/iam-role.tf
@@ -26,7 +26,6 @@ resource "aws_iam_instance_profile" "ec2-profile" {
 resource "aws_iam_role_policy" "ec2-role-policy" {
   name = "EC2-Access-to-S3-Terraform-Bucket"
   role = "${aws_iam_role.iam_role-for-ec2.id}"
-  path = "/"
   policy = <<EOF
 {
   "Version": "2012-10-17",
