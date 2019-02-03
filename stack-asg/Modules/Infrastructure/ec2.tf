4a3e00b76434642041ea953719c69a184a1a6208
Raghu K <raghuk.vit@gmail.com>
2019-02-03 07:41:47 +0530

    --- Auto-Git Commit ---

--- stack-asg/Modules/Infrastructure/ec2.tf
+++ stack-asg/Modules/Infrastructure/ec2.tf
@@ -24,17 +24,6 @@ resource "aws_autoscaling_group" "asg" {
   launch_configuration      = "${aws_launch_configuration.launch-config.name}"
   vpc_zone_identifier       = ["${var.PUBLIC_SUBNETS}"]
 
-  initial_lifecycle_hook {
-    name                 = "foobar"
-    default_result       = "CONTINUE"
-    heartbeat_timeout    = 2000
-    lifecycle_transition = "autoscaling:EC2_INSTANCE_LAUNCHING"
-
-    notification_metadata = <<EOF
-{
-  "foo": "bar"
-}
-EOF
 
     notification_target_arn = "arn:aws:sqs:us-east-1:444455556666:queue1*"
     role_arn                = "arn:aws:iam::123456789012:role/S3Access"
