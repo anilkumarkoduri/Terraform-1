56872ac13299dfe8048dfc3ebc612ad221981737
Raghu K <raghuk.vit@gmail.com>
2019-02-04 06:18:14 +0530

    --- Auto-Git Commit ---

--- stack-asg/Modules/Database/s3.tf
+++ stack-asg/Modules/Database/s3.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket_object" "object" {
   bucket = "terra-citb34"
-  key    = "new_object_key"
+  key    = "DE"
   source = "path/to/file"
   etag   = "${md5(file("path/to/file"))}"
 }
