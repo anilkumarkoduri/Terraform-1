4f07f98ba6676961aa90da806ddcaf5cdbf76b65
Raghu K <raghuk.vit@gmail.com>
2019-02-04 06:26:35 +0530

    --- Auto-Git Commit ---

--- stack-asg/Modules/Database/rds.tf
+++ stack-asg/Modules/Database/rds.tf
@@ -34,7 +34,7 @@ resource "null_resource" "db-values-export" {
 
   provisioner "local-exec" {
     command = <<EOF
-    echo -e "RDS_ENDPOINT:${} \n RDS_USERNAME=\n RDS_PASSWORD= \nRDS_DBNAME="
+    echo -e "RDS_ENDPOINT:${aws_db_instance.default.address}\nRDS_USERNAME=\n RDS_PASSWORD= \nRDS_DBNAME="
     EOF
   }
 }
