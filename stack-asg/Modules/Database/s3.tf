3f02079dd5e779b4e6172392215181655f645c91
Raghu K <raghuk.vit@gmail.com>
2019-02-04 16:27:37 +0530

    --- Auto-Git Commit ---

--- stack-asg/Modules/Database/s3.tf
+++ stack-asg/Modules/Database/s3.tf
@@ -2,5 +2,5 @@ resource "aws_s3_bucket_object" "object" {
   bucket = "terra-citb34"
   key    = "DB-Details/dev-db-details"
   source = "dev-db-details"
-  depends = ["null_resource."]
+  depends = ["null_resource.db-values-export"]
 }
