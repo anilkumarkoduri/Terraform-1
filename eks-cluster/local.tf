f4b6b2fd95017d6216ea2c9354d1d15ee1312163
Raghu K <raghuk.vit@gmail.com>
2019-02-21 05:41:26 +0530

    --- Auto-Git Commit ---

--- eks-cluster/local.tf
+++ eks-cluster/local.tf
@@ -1,4 +1,6 @@
 resource "null_resource" "example1" {
+
+    depends_on = ["aws_autoscaling_group.demo"] 
   provisioner "local-exec" {
     command = <<EOT
       aws eks update-kubeconfig --name ekscluster
