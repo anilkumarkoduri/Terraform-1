d497b545929a39ba5ac2468e2adf2c5967f21071
Raghu K <raghuk.vit@gmail.com>
2019-02-23 21:24:50 +0530

    --- Auto-Git Commit ---

--- eks-cluster/local.tf
+++ eks-cluster/local.tf
@@ -4,6 +4,7 @@ resource "null_resource" "example1" {
   provisioner "local-exec" {
     command = <<EOT
       aws eks update-kubeconfig --name ekscluster
+      
       kubectl create -f config.yml
     EOT
   }
