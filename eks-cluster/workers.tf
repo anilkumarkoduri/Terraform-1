e81d653ed6993efc6d32dc81758868402fdcae0c
Raghu K <raghuk.vit@gmail.com>
2019-02-23 21:19:30 +0530

    --- Auto-Git Commit ---

--- eks-cluster/workers.tf
+++ eks-cluster/workers.tf
@@ -57,3 +57,25 @@ resource "aws_autoscaling_group" "demo" {
   }
 }
 
+locals {
+  config_map_aws_auth = <<CONFIGMAPAWSAUTH
+
+
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: aws-auth
+  namespace: kube-system
+data:
+  mapRoles: |
+    - rolearn: ${aws_iam_role.demo-node.arn}
+      username: system:node:{{EC2PrivateDNSName}}
+      groups:
+        - system:bootstrappers
+        - system:nodes
+CONFIGMAPAWSAUTH
+}
+
+output "config_map_aws_auth" {
+  value = "${local.config_map_aws_auth}"
+}
