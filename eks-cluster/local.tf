16ed1c4e48db7199c754bee0bb6b20262f572865
Raghu K <raghuk.vit@gmail.com>
2019-02-23 21:31:51 +0530

    --- Auto-Git Commit ---

--- eks-cluster/local.tf
+++ eks-cluster/local.tf
@@ -4,7 +4,7 @@ resource "null_resource" "example1" {
   provisioner "local-exec" {
     command = <<EOT
       aws eks update-kubeconfig --name ekscluster
-      echo "${local.config_map_aws_auth}" >/tmp/sample.yml
+      echo "${local.config_map_aws_auth}" 
       kubectl create -f config.yml
     EOT
   }
